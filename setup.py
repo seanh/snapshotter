67de9f4a30c18c13ad64f46a1b7a21efea6de963
Sean Hammond <git@seanh.cc>
2016-03-20 18:53:02 +0000

    Release version 1.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='1.0.2',
+    version='1.0.4',
 
     description='Very easy incremental snapshot backups using rsync',
     long_description=long_description,
